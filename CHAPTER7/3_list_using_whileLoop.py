8fe537e7ca00c277bbe0ff396150e58643b435b2
dvlpr_kaushik <freakyboyftw@gmail.com>
2024-08-13 12:24:10 +0530

    CHAPTER7 PROGRAMS

---
+++ CHAPTER7/3_list_using_whileLoop.py
@@ -0,0 +1,7 @@
+names = ["Kaushik", "Ananya", "Rohit", "Priya", "Siddharth",
+         "Meera", "Vikram", "Aisha", "Nikhil", "Sonal"]
+listLen = len(names)
+i = 0
+while (i < listLen):
+    print(names[i])
+    i += 1
---
+++ CHAPTER7/4_forLoopRange.py
@@ -0,0 +1,3 @@
+for item in range(0,20,2): #start,end,step to skip (step size)
+    print(item)
+
